bdcf0496f390201c9abaa2058597f8980ea82883
Abhishek Bhattacharjee <ahishek1250@gmail.com>
2023-11-14 14:31:13 +0530

    Implemented routes and gave basic styling to AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,37 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import AppLayout from "./ui/AppLayout";
+import SignUpPage from "./pages/SignUpPage";
+import SignUpSuccessPage from "./pages/SignUpSucessPage";
+import PageNotFound from "./pages/PageNotFound";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Navigate to="/signup" replace />,
+      },
+      {
+        path: "/signup",
+        element: <SignUpPage />,
+      },
+      {
+        path: "/signup-success",
+        element: <SignUpSuccessPage />,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <PageNotFound />,
+  },
+]);
+
 export default function App() {
-  return <div>Hi there it&mdash;s App</div>;
+  return <RouterProvider router={router} />;
 }
